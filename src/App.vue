a18251062674a6d34e30185f8b1e1b4c65a3f36e
Viktor Volodin <volvi99@mail.ru>
2022-05-18 02:09:30 +0300

    Added basic components: 'Card', 'Input' and their styles. Added the basic fonts configuration.

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-</template>
-
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-
+import Card from "./components/container/Card.vue";
+import Input from "./components/interaction/Input.vue";
+@Options({
+  components: {
+    Card,
+    Input,
+  },
+})
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+@import "./styles/colors.scss";
+@import "./styles/fonts.scss";
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Source Sans Pro", sans-serif;
+  font-style: normal;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $text-color;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/interaction/Input.vue
@@ -0,0 +1,44 @@
+<template>
+  <textarea
+    v-if="textarea"
+    :style="sizes"
+    :placeholder="placeholder"
+    class="input"
+  >
+  </textarea>
+  <input v-else :style="sizes" :placeholder="placeholder" class="input" />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    textarea: {
+      type: Boolean,
+      default: false,
+    },
+    sizes: {
+      type: Object,
+      default: {
+        width: "17.75rem",
+        height: "2.25rem",
+      },
+    },
+    placeholder: {
+      type: String,
+    },
+  },
+})
+export default class Card extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+@import "../../styles/colors.scss";
+
+.input {
+  border: 1px solid $input-border;
+  border-radius: 0.25rem;
+  padding: 0.65rem 1rem;
+}
+</style>
